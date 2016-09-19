75072a3f2fa9e95ce21beab3670b770deb0a3b35
Marcel Freinbichler <marcel@freinbichler.me>
2016-09-19 16:29:34 +0200

    add touchforcechange event support for iOS 10

--- 3dtouch.js
+++ 3dtouch.js
@@ -3,8 +3,6 @@ var forceValueOutput = document.getElementById('forceValue');
 var background = document.getElementById('background');
 var touch = null;
 
-addForceTouchToElement(element);
-
 function onTouchStart(e) {
   e.preventDefault();
   checkForce(e);
@@ -17,19 +15,29 @@ function onTouchMove(e) {
 
 function onTouchEnd(e) {
   e.preventDefault();
+  setTimeout(renderElement.bind(null, 0), 10);
   touch = null;
 }
 
+// use timeout-based method only on devices lower than iOS 10
 function checkForce(e) {
-  touch = e.touches[0];
-  setTimeout(refreshForceValue.bind(touch), 10);
+  if(getiOSVersion() < 10) {
+    touch = e.touches[0];
+    setTimeout(refreshForceValue.bind(touch), 10);
+  }
 }
 
-function checkMacForce(e) {
-  // max value for trackpad is 3.0 compare to 1.0 on iOS
-  renderElement(e.webkitForce/3);
+// the maximum force value of a touch event is 1
+function onTouchForceChange(e) {
+  renderElement(e.changedTouches[0].force);
 }
 
+// the maximum force value of a click event is 3
+function onClickForceChange(e) {
+  renderElement(e.webkitForce / 3);
+}
+
+// iOS versions lower than iOS 10 do not support the touchforcechange event, so refresh manually
 function refreshForceValue() {
   var touchEvent = this;
   var forceValue = 0;
@@ -43,16 +51,30 @@ function refreshForceValue() {
   renderElement(forceValue);
 }
 
+// update the element according to the force value (between 0 and 1)
 function renderElement(forceValue) {
   element.style.webkitTransform = 'translateX(-50%) translateY(-50%) scale(' + (1 + forceValue * 1.5) + ')';
   background.style.webkitFilter = 'blur(' + forceValue * 30 + 'px)';
   forceValueOutput.innerHTML = 'Force: ' + forceValue.toFixed(4);
 }
 
+// add event listeners
 function addForceTouchToElement(elem) {
   elem.addEventListener('touchstart', onTouchStart, false);
   elem.addEventListener('touchmove', onTouchMove, false);
   elem.addEventListener('touchend', onTouchEnd, false);
-  elem.addEventListener('webkitmouseforcewillbegin', checkMacForce, false);
-  elem.addEventListener('webkitmouseforcechanged', checkMacForce, false);
+  elem.addEventListener('webkitmouseforcechanged', onClickForceChange, false);
+  elem.addEventListener('touchforcechange', onTouchForceChange, false);
 }
+
+// get iOS version number
+function getiOSVersion() {
+  if(window.navigator.userAgent.match( /iP(hone|od|ad)/)){
+    var iOSVersion = parseFloat(String(window.navigator.userAgent.match(/[0-9]*_[0-9]/)).split('_')[0]+'.'+String(window.navigator.userAgent.match(/[0-9]_[0-9]/)).split('_')[1]);
+    return iOSVersion;
+  }
+
+  return false;
+}
+
+addForceTouchToElement(element);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 3D Touch JavaScript Implementation
-Visit [this page](http://freinbichler.me/apps/3dtouch/) on your 3D Touch enabled device (for example, iPhone 6S or newer) to try it.
+Visit [this page](http://freinbichler.me/apps/3dtouch/) on your 3D Touch enabled device (for example, iPhone 6s or newer) to try it.
+
+This project uses the `touchforcechange` event on iOS 10 and uses a timeout-based version as a fallback on older iOS versions.
